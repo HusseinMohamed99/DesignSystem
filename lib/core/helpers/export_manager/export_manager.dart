00d9777143be1335fe6c9f3ef1d7667c63ff5538
DESKTOP-O90U05F\pc <value9660@gmail.com>
2024-09-29 13:35:09 +0300

    - Added flutter_screenutil Package - Upgrade main.dart - Added Responsive Font Size - Added Spacing

--- lib/core/helpers/export_manager/export_manager.dart
+++ lib/core/helpers/export_manager/export_manager.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+part './../../../main.dart';
 part './../adaptive/adaptive_layout.dart';
+part './../responsive/responsive_font_size.dart';
+part './../responsive/spacing.dart';
