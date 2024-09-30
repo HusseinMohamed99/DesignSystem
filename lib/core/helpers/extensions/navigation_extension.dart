60d13025fc02a15aaed85326dbc0588f9fa4fa2d
DevHussein\husse <https://github.com/HusseinMohamed99>
2024-09-30 18:20:00 +0300

    Add VSCode settings and enhance Flutter extensions
    - Created a new `.vscode/settings.json` file to configure Better Comments extension tags with specific colors and styles.
    - Added `ui_extension.dart` and `widget_extension.dart` to the export manager and updated the part directives accordingly.
    - Enhanced the `navigation_extension.dart` with additional methods for pushing routes and accessing the ScaffoldMessenger.
    - Introduced new UI extensions for padding, margin, border radius, dividers, and SizedBox widgets.
    - Added widget extensions for padding, centering, and clipping widgets with rounded corners.
    - Removed the `spacing.dart` file as its functionality is now covered by the new UI extensions.

--- lib/core/helpers/extensions/navigation_extension.dart
+++ lib/core/helpers/extensions/navigation_extension.dart
@@ -1,22 +1,29 @@
 part of './../export_manager/export_manager.dart';
 
 extension Navigation on BuildContext {
-  Future<dynamic> pushNamed(String routeName, {Object? arguments}) {
+  Future<T?> pushNamed<T>(String routeName, {Object? arguments}) {
     return Navigator.of(this).pushNamed(routeName, arguments: arguments);
   }
 
-  Future<dynamic> pushReplacementNamed(String routeName, {Object? arguments}) {
+  Future<T?> push<T>(Widget page) {
+    return Navigator.push(this, MaterialPageRoute(builder: (context) => page));
+  }
+
+  Future<T?> pushReplacementNamed<T>(String routeName, {Object? arguments}) {
     return Navigator.of(this)
         .pushReplacementNamed(routeName, arguments: arguments);
   }
 
-  Future<dynamic> pushNamedAndRemoveUntil(String routeName,
+  Future<T?> pushNamedAndRemoveUntil<T>(String routeName,
       {Object? arguments, required RoutePredicate predicate}) {
     return Navigator.of(this)
         .pushNamedAndRemoveUntil(routeName, predicate, arguments: arguments);
   }
 
-  void pop() => Navigator.of(this).pop();
+  void pop<T>([T? result]) => Navigator.pop(this, result);
+
+  // ***Access the scaffold messenger for showing snack bars **
+  ScaffoldMessengerState get scaffoldMessenger => ScaffoldMessenger.of(this);
 }
 
 extension StringExtension on String? {
--- lib/core/helpers/responsive/spacing.dart
+++
@@ -1,4 +0,0 @@
-part of './../export_manager/export_manager.dart';
-
-SizedBox verticalSpacing(double height) => SizedBox(height: height.h);
-SizedBox horizontalSpacing(double width) => SizedBox(width: width.w);
