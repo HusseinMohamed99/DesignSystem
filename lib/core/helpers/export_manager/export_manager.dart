0d5395367a4b420b18da1775d5eece25c766e191
DevHussein\husse <https://github.com/HusseinMohamed99>
2024-09-30 14:26:32 +0300

    Refactor export manager and add responsive size and snack bar extensions
    - Reorganized the `export_manager.dart` file to include new extensions:
      - Added `responsive_size_extension.dart` to the export manager.
      - Added `snack_bar_extension.dart` to the export manager.
    - Created `responsive_size_extension.dart` to provide responsive size properties for `BuildContext`.
    - Created `snack_bar_extension.dart` to simplify the process of showing snack bars in the app.
    - Updated the `export_manager.dart` file to reflect the new part files and maintain proper order.

--- lib/core/helpers/export_manager/export_manager.dart
+++ lib/core/helpers/export_manager/export_manager.dart
@@ -5,8 +5,8 @@ import 'package:responsive_framework/responsive_framework.dart';
 part './../../../main.dart';
 part './../adaptive/adaptive_layout.dart';
 part './../app_string/validation_message.dart';
+part './../extensions/responsive_size_extension.dart';
+part './../functions/remove_spacing.dart';
 part './../responsive/responsive_font_size.dart';
 part './../responsive/spacing.dart';
 part './../validations/validation_manager.dart';
-
-part './../functions/remove_spacing.dart';
