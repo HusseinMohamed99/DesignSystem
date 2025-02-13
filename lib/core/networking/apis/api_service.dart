b198088543e5e90b293aff00c77e96d3d1bd0f36
Hussein Mohamed <84459939+HusseinMohamed99@users.noreply.github.com>
2025-02-13 15:09:29 +0200

    Refactor string quotes for consistency in API error handling and service files; update import paths in generated files

--- lib/core/networking/apis/api_service.dart
+++ lib/core/networking/apis/api_service.dart
@@ -3,7 +3,7 @@ import 'package:retrofit/retrofit.dart';
 
 part 'api_service.g.dart';
 
-@RestApi(baseUrl: "https://shurrlyapp-production.up.railway.app/api/v1/")
+@RestApi(baseUrl: 'https://shurrlyapp-production.up.railway.app/api/v1/')
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
 }
